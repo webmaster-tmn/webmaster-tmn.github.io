3e06fc943d9ebcc603b836c85d35f022fe150318
denis_tmn <denis.derjabin@yandex.ru>
2019-06-14 01:39:13 +0500

    remove leon landings

--- matchoftheday/js/script.js
+++
@@ -1,8 +0,0 @@
-  
-function affLink() {
-      var url = "http://affsnetwork.com/partner928/registration";
-      var curUrl = window.location.href;
-      var affID = curUrl.match(/\d+$/);
-      var regUrl = url + "?wm=" + affID;
-      window.location.href = affID ? regUrl : url;
-  }
